c0434592d7db4a33bc59ec4984fbcee2f9cf6e9d
SudharshanMyilsamy <141046702+SudharshanMyilsamy@users.noreply.github.com>
2025-04-02 17:12:58 +0530

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Sudharshan Myilsamy. <br />
+        &copy; 2025 Sudharshan Myilsamy. <br />
         All rights reserved.
       </p>
     </section>
